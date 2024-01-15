7abe89fa3587f32d6a0bdc37848f5e9d847d0878
Matt Vogel <mainmoniker@googlemail.com>
2024-01-15 17:14:07 -0500

    feat: better datalog

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,30 @@
+  (() => {
+    function findUpcomingBirthdays(data) {
+      const today = new Date();
+      const twoWeeksLater = new Date(today.getTime() + 14 * 24 * 60 * 60 * 1000);
+      const upcomingBirthdays = [];
+  
+      for (const person in data) {
+          if (data.hasOwnProperty(person)) {
+              const birthdayStr = data[person].birthday;
+              const birthday = new Date(birthdayStr);
+              // Adjust the birthday year to this year for comparison
+              birthday.setFullYear(today.getFullYear());
+  
+              // Check if the birthday has already occurred this year, if so, set to next year.
+              if (birthday < today) {
+                  birthday.setFullYear(today.getFullYear() + 1);
+              }
+  
+              if (birthday >= today && birthday <= twoWeeksLater) {
+                  upcomingBirthdays.push({ name: person, birthday: birthday.toISOString() });
+              }
+          }
+      }
+  
+      return upcomingBirthdays;
+    }
 
-(() => {
     function parseStringToDate(dateString) {
       const defaultYear = 1980; // Set a default year, you can change it to whatever you prefer
     
@@ -37,27 +62,72 @@
       
       return dateObject;
     }
-    
-   
-    
-    let query = `[:find (pull ?r [* {:block/page ...}])
-                        :in $ ?namespace
-                        :where 
-                          [?p :node/title ?namespace]
-                          [?r :block/refs ?p]
-                          [?r :block/page ?pr]
-                          ]`;
+    let query = `[:find 
+                  (pull ?node [:block/string :block/uid])
+                  (pull ?PAGE [:node/title])
+                  (pull ?CONTACTdec [[:node/title :as :contact-title] [:block/string :as :contact-string] :block/refs {:block/refs ...}])
+                :where
+                  [?Birthday-Ref :node/title "Birthday"]
+                  [?Tags-Ref :node/title "Tags"]
+                  [?person-Ref :node/title "person"]
+                  [?ContactFrequency-Ref :node/title "Contact Frequency"]
+                  [?node :block/refs ?Birthday-Ref]
+                  [?node :block/page ?PAGE]
+                  [?PEOPLEdec :block/parents ?PAGE]
+                  [?PEOPLEdec :block/refs ?Tags-Ref]
+                  [?PEOPLEdec :block/refs ?person-Ref]
+                  [?CONTACTdec :block/parents ?PAGE]
+                  [?CONTACTdec :block/refs ?ContactFrequency-Ref]
+                  (not
+                    [?not_populated-Ref :node/title "not_populated"]
+                    [?PEOPLEdec :block/refs ?not_populated-Ref]
+                  )
+                ]`;
   
-    let result = window.roamAlphaAPI.q(query,'Birthday').flat();
-    result.forEach(block => {
-      const dateString = block.string.split("::", 2)[1].replace(/\[|\]/g, '');
+    let results = window.roamAlphaAPI.q(query).flat();
+    let mappedResults = {};
+
+    // Iterate through results three at a time
+    for (let i = 0; i < results.length; i += 3) {
+      let node = results[i];
+      let page = results[i + 1];
+      let contact = results[i + 2];
+
+      // Merge the node and page objects
+      let mergedObject = {
+        ...node, // Spread the properties of the node object
+        ...page,  // Spread the properties of the page object
+        ...contact
+      };
+
+      // parse birthday date
+      const dateString = mergedObject.string.split("::", 2)[1].replace(/\[|\]/g, '');
       // console.log(dateString);
-      const birthdays = parseStringToDate(dateString.trim())
-      const page = block.page.title
+      // mergedObject.birthday = parseStringToDate(dateString.trim())
       // console.log(birthdays);
-      console.log(page);
-      
-      
-    });
-    // return result;
-  })();
+
+      const filteredObjects = mergedObject.refs.filter(obj => {
+        // Assuming there's only one key-value pair in each object
+        return Object.values(obj).some(value => value.toLowerCase().includes("list"));
+       });
+       // If no object with "list" is found, set "C List"
+        if (filteredObjects.length === 0) {
+          mergedObject.contact = "C List";
+        } else {
+          mergedObject.contact = filteredObjects[0]['contact-title']
+        }
+
+        mappedResults[mergedObject['title']] = {
+          "birthday":parseStringToDate(dateString.trim()),
+          "contact_list":mergedObject.contact,
+          "birthday_UID":mergedObject['uid'],
+          "last_contact":null
+        }
+       
+      // mappedResults.push(mergedObject);
+    }
+
+    return mappedResults;
+  })(); 
+
+
