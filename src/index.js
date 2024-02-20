3aa802a8209fe9df6070d6d970c8aac28fe8e4f4
Matt Vogel <mainmoniker@googlemail.com>
2024-02-20 09:13:09 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,9 @@ import { getAllPeople,
 import { createLastWeekCalls, createLastMonthCalls, createPersonTemplates, createCallTemplates } from './components/call_templates';
 
 const testing = false
-const version = "v0.9.5.1"
+const version = "v0.9.5.2"
 
 const plugin_title = "Roam CRM"
-const ts2 = 1708980444000
 
 let googleLoadedHandler;
 
@@ -187,120 +186,117 @@ async function onload({ extensionAPI }) {
   extensionAPI.settings.panel.create(panelConfig);
   const ts1 = new Date().getTime();
 
-  if (ts1 < ts2) {
-    const people = await getAllPeople()
-    // add left sidebar button
-    crmbutton(extensionAPI)
-    if (testing) {
-      displayBirthdays(people, '01-19-2024')
-    } else {
-      displayBirthdays(people, getLastBirthdayCheckDate(extensionAPI))
-    }
-
-    // update last birthday check since it's already happened
-    extensionAPI.settings.set(
-      'last-birthday-check-date',
-      window.roamAlphaAPI.util.dateToPageUid(new Date))
-
-    
-    // bring in the events, this should rely on getLastBirthdayCheckDate to avoid duplicates
-    // listen for the google extension to be loaded
-    if (window.roamjs?.extension?.google) {
-      await getEventInfo(people, extensionAPI, testing)
-    } else {
-      googleLoadedHandler = createGoogleLoadedHandler(people, extensionAPI);
-      document.body.addEventListener('roamjs:google:loaded', googleLoadedHandler);
-     
-    }
-
-    // always set people pages to hide DONE
-    // TODO see if they want more granulity
-    people.forEach(async page =>  {
-      await setDONEFilter(page.title)
-    });
-
-    // Command Palette Sidebar - Close first block
-    extensionAPI.ui.commandPalette.addCommand(
-      {
-        label: 'Sidebar - Close first block',
-        "disable-hotkey": false,
-        callback: async () => {
-          async function removeWindow(w) {
-            window.roamAlphaAPI.ui.rightSidebar.removeWindow(
-              {
-                "window":
-                {
-                  "type": w['type'],
-                  "block-uid": w['block-uid'] || w['page-uid'] || w['mentions-uid']
-                }
-              }
-            )
-          }
-
-          let sidebarWindows = window.roamAlphaAPI.ui.rightSidebar.getWindows();
-          const filteredBlocks = sidebarWindows.filter(obj => !obj["pinned?"]);
-          if (filteredBlocks.length > 0) {
-            filteredBlocks.sort((a, b) => a.order - b.order);
-            await removeWindow(filteredBlocks[0])
-          }
-
-        }
-      }
-    )
-    // Command Palette Sidebar - Cursor in first block
-    extensionAPI.ui.commandPalette.addCommand(
-      {
-        label: 'Sidebar - Cursor in first block',
-        "disable-hotkey": false,
-        callback: async () => {
-          let sidebarWindows = window.roamAlphaAPI.ui.rightSidebar.getWindows();
-
-          if (sidebarWindows.length > 0) {
-            let first = sidebarWindows[0];
-            if (first.type == "block") {
-              window.roamAlphaAPI.ui.setBlockFocusAndSelection(
-                {
-                  location: {
-                    "block-uid": first['block-uid'],
-                    "window-id": first['window-id']
-                  }
-                })
-            } else if (first.type == "outline") {
-              let query = `[:find (pull ?e [:block/string :block/uid :block/children :block/order {:block/children ...}])
-                              :in $ ?uid
-                              :where 
-                    [?e :block/uid ?uid]
-                    ]`;
-
-              let result = window.roamAlphaAPI.q(query, first['page-uid']).flat();
-              const selectedObject = result[0].children.find(obj => obj.order === 0);
-              window.roamAlphaAPI.ui.setBlockFocusAndSelection(
-                {
-                  location: {
-                    "block-uid": selectedObject['uid'],
-                    "window-id": first['window-id']
-                  }
-                })
-            }
-          }
-
-        }
-      }
-    )
-
-    // agenda addr pull watch
-    window.roamAlphaAPI.data.addPullWatch(
-      pullPattern,
-      entity,
-      pullFunction
-    )
-
-    if (!testing) {
-      console.log(`load ${plugin_title} plugin`);
-    }
+  const people = await getAllPeople()
+  // add left sidebar button
+  crmbutton(extensionAPI)
+  if (testing) {
+    displayBirthdays(people, '01-19-2024')
   } else {
-    showToast(`Failed to run an old version of ${plugin_title}.`, "DANGER");
+    displayBirthdays(people, getLastBirthdayCheckDate(extensionAPI))
   }
+
+  // update last birthday check since it's already happened
+  extensionAPI.settings.set(
+    'last-birthday-check-date',
+    window.roamAlphaAPI.util.dateToPageUid(new Date))
+
+  
+  // bring in the events, this should rely on getLastBirthdayCheckDate to avoid duplicates
+  // listen for the google extension to be loaded
+  if (window.roamjs?.extension?.google) {
+    await getEventInfo(people, extensionAPI, testing)
+  } else {
+    googleLoadedHandler = createGoogleLoadedHandler(people, extensionAPI);
+    document.body.addEventListener('roamjs:google:loaded', googleLoadedHandler);
+    
+  }
+
+  // always set people pages to hide DONE
+  // TODO see if they want more granulity
+  people.forEach(async page =>  {
+    await setDONEFilter(page.title)
+  });
+
+  // Command Palette Sidebar - Close first block
+  extensionAPI.ui.commandPalette.addCommand(
+    {
+      label: 'Sidebar - Close first block',
+      "disable-hotkey": false,
+      callback: async () => {
+        async function removeWindow(w) {
+          window.roamAlphaAPI.ui.rightSidebar.removeWindow(
+            {
+              "window":
+              {
+                "type": w['type'],
+                "block-uid": w['block-uid'] || w['page-uid'] || w['mentions-uid']
+              }
+            }
+          )
+        }
+
+        let sidebarWindows = window.roamAlphaAPI.ui.rightSidebar.getWindows();
+        const filteredBlocks = sidebarWindows.filter(obj => !obj["pinned?"]);
+        if (filteredBlocks.length > 0) {
+          filteredBlocks.sort((a, b) => a.order - b.order);
+          await removeWindow(filteredBlocks[0])
+        }
+
+      }
+    }
+  )
+  // Command Palette Sidebar - Cursor in first block
+  extensionAPI.ui.commandPalette.addCommand(
+    {
+      label: 'Sidebar - Cursor in first block',
+      "disable-hotkey": false,
+      callback: async () => {
+        let sidebarWindows = window.roamAlphaAPI.ui.rightSidebar.getWindows();
+
+        if (sidebarWindows.length > 0) {
+          let first = sidebarWindows[0];
+          if (first.type == "block") {
+            window.roamAlphaAPI.ui.setBlockFocusAndSelection(
+              {
+                location: {
+                  "block-uid": first['block-uid'],
+                  "window-id": first['window-id']
+                }
+              })
+          } else if (first.type == "outline") {
+            let query = `[:find (pull ?e [:block/string :block/uid :block/children :block/order {:block/children ...}])
+                            :in $ ?uid
+                            :where 
+                  [?e :block/uid ?uid]
+                  ]`;
+
+            let result = window.roamAlphaAPI.q(query, first['page-uid']).flat();
+            const selectedObject = result[0].children.find(obj => obj.order === 0);
+            window.roamAlphaAPI.ui.setBlockFocusAndSelection(
+              {
+                location: {
+                  "block-uid": selectedObject['uid'],
+                  "window-id": first['window-id']
+                }
+              })
+          }
+        }
+
+      }
+    }
+  )
+
+  // agenda addr pull watch
+  window.roamAlphaAPI.data.addPullWatch(
+    pullPattern,
+    entity,
+    pullFunction
+  )
+
+  if (!testing) {
+    console.log(`load ${plugin_title} plugin`);
+  }
+  
 
 }
 
